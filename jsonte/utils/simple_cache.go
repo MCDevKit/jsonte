e32b6dc5b94c892e80ebe287dc5d3975ad267f80
stirante <brzozowski.s.piotr@gmail.com>
2025-07-02 17:00:52 +0200

    Add concurrency support to cache operations with mutexes

--- jsonte/utils/simple_cache.go
+++ jsonte/utils/simple_cache.go
@@ -1,43 +1,67 @@
 package utils
 
-var cache = map[string]CacheBucket{}
+import "sync"
+
+var (
+	cache   = map[string]*CacheBucket{}
+	cacheMu sync.RWMutex
+)
 
 // CacheBucket is a bucket of cache entries
 type CacheBucket struct {
 	Items map[string]*interface{}
+	mu    sync.RWMutex
 }
 
 // CreateCacheBucket creates a cache bucket
 func CreateCacheBucket(bucket string) {
+	cacheMu.Lock()
+	defer cacheMu.Unlock()
 	if _, ok := cache[bucket]; !ok {
-		cache[bucket] = CacheBucket{
+		cache[bucket] = &CacheBucket{
 			Items: map[string]*interface{}{},
 		}
 	}
 }
 
 // PutCache puts a cache entry
-func PutCache(bucket string, key string, value interface{}) {
-	if b, ok := cache[bucket]; ok {
+func PutCache(bucket, key string, value interface{}) {
+	cacheMu.RLock()
+	b, ok := cache[bucket]
+	cacheMu.RUnlock()
+	if ok {
+		b.mu.Lock()
 		b.Items[key] = &value
+		b.mu.Unlock()
 	} else {
 		BadDeveloperError("Cache bucket not found!")
 	}
 }
 
 // EvictCache evicts a cache entry
-func EvictCache(bucket string, key string) {
-	if b, ok := cache[bucket]; ok {
+func EvictCache(bucket, key string) {
+	cacheMu.RLock()
+	b, ok := cache[bucket]
+	cacheMu.RUnlock()
+	if ok {
+		b.mu.Lock()
 		if _, ok := b.Items[key]; ok {
 			delete(b.Items, key)
 		}
+		b.mu.Unlock()
 	}
 }
 
 // GetCache gets a cache entry
-func GetCache(bucket string, key string) *interface{} {
-	if b, ok := cache[bucket]; ok {
-		if i, ok := b.Items[key]; ok {
+func GetCache(bucket, key string) *interface{} {
+	cacheMu.RLock()
+	b, ok := cache[bucket]
+	cacheMu.RUnlock()
+	if ok {
+		b.mu.RLock()
+		i, ok := b.Items[key]
+		b.mu.RUnlock()
+		if ok {
 			return i
 		}
 	}
